274752b1a10e063a71951f98d4e1d33e30f45238
Rony163 <minhajulislamrony43@gmail.com>
2021-10-12 13:12:35 +0600

    bootstrap responsive

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import './Service.css';
 
 const Service = ({ service }) => {
     // const {service} = props;
     const { name, img, price, description } = service;
     return (
-        <div>
+        <div className="service">
             <img src={img} alt="" />
             <h3>{name}</h3>
             <h5>{price}</h5>
-            <p>{description}</p>
+            <p className="px-3">{description}</p>
         </div>
     );
 };
