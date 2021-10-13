241f8d0aa89b94880cf3e7b773465171e74c7341
Rony163 <minhajulislamrony43@gmail.com>
2021-10-13 13:59:46 +0600

    adding hader

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Service.css';
 
 const Service = ({ service }) => {
     // const {service} = props;
-    const { name, img, price, description } = service;
+    const { id, name, img, price, description } = service;
     return (
-        <div className="service">
+        <div className="service pb-3">
             <img src={img} alt="" />
             <h3>{name}</h3>
             <h5>{price}</h5>
             <p className="px-3">{description}</p>
+            <Link to={`/booking/${id}`}><button className="btn btn-warning">Book {name.toLowerCase()}</button></Link>
         </div>
     );
 };
