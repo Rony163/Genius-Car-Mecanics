b341c9be7733caf128a76c3b739f4851de03b9f5
Rony163 <minhajulislamrony43@gmail.com>
2021-10-24 15:35:27 +0600

    get data from database

--- src/Pages/Booking/Booking/Booking.js
+++ src/Pages/Booking/Booking/Booking.js
@@ -3,13 +3,15 @@ import { useParams } from 'react-router';
 
 const Booking = () => {
     const { serviceId } = useParams();
+
     const [services, setServices] = useState([]);
     useEffect(() => {
-        fetch('services.json')
+        fetch('https://raw.githubusercontent.com/Rony163/Genius-Car-Mecanics/main/public/services.json')
             .then(res => res.json())
             .then(data => setServices(data))
     }, [])
-    console.log(services.length)
+    console.log(services.length);
+
     return (
         <div>
             <h1>This is Booking {serviceId}</h1>
--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -4,14 +4,14 @@ import './Service.css';
 
 const Service = ({ service }) => {
     // const {service} = props;
-    const { id, name, img, price, description } = service;
+    const { _id, name, img, price, description } = service;
     return (
         <div className="service pb-3">
             <img src={img} alt="" />
             <h3>{name}</h3>
             <h5>{price}</h5>
             <p className="px-3">{description}</p>
-            <Link to={`/booking/${id}`}><button className="btn btn-warning">Book {name.toLowerCase()}</button></Link>
+            <Link to={`/booking/${_id}`}><button className="btn btn-warning">Book {name.toLowerCase()}</button></Link>
         </div>
     );
 };
